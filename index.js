32a465327001324ebe09cad299b886e216814dc2
Sarwar-Putul <sarwarputul@gmail.com>
2021-03-24 04:21:24 +0600

    module-45  done

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express')
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const users= ['Sarwar', 'Nahiyaan', 'Putul', 'Krishaan', 'Betu','Sharaf', 'Rafi'];
 
 app.get('/', (req, res) => {
-    res.send('I know how to open Node.....YaY');
+const friut= {
+    product:'ada',
+    price: 120
+}
+    res.send(friut);
 })
 
+app.get('/users/:id',(req, res) =>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+    
+})
+
+//Post
+
+app.post('/addUser',(req, res) =>{
+    //Save to database
+    const user = req.body;
+    user.id =23
+    res.send(user);
+})
 
 
 app.listen(3000, () => console.log('Listening to port 3000'))
